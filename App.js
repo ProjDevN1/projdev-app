50ab675232947957d8ec93ccbbe68dab832e67e1
Niko <nlehtonen1@gmail.com>
2023-02-11 10:14:14 +0200

    Added dev screen + cleanup

--- App.js
+++ App.js
@@ -1,29 +1,25 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { STYLES } from './constants/styles'
 
 
-{/*Under here imports for screens contained in other files in the screens folder*/}
+//Under here imports for screens contained in other files in the screens folder
 
 import LoginScreen from './screens/LoginScreen'
 import HomeScreen from './screens/HomeScreen';
+import DevScreen from './screens/DevScreen';
 
-{/*Initializes the stack navigator module, used to navigate between screens*/}
+//Initializes the stack navigator module, used to navigate between screens
 const Stack = createNativeStackNavigator();
 
 
-{/*This is the main app function*/}
+//This is the main app function
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{title: 'Applikaatio perkele'}}
-        />
+        <Stack.Screen name="Dev" component={DevScreen}/>
+        <Stack.Screen name="Login" component={LoginScreen}/>
         <Stack.Screen name="Home" component={HomeScreen} /> 
       </Stack.Navigator>
     </NavigationContainer>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Text, View } from 'react-native';
 import { STYLES } from '../constants/styles'
 
 {/*This file contains the code for the home screen, which is imported in the main app.js file*/}
