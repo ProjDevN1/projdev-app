8418f5952324a2347a991d35348b5ec5ee2caa5a
3s Studio <linardsliepenieks@gmail.com>
2023-03-07 19:03:25 +0200

    eevi code review

--- screens/AddPicsAndInfo.js
+++ screens/AddPicsAndInfo.js
@@ -1,47 +1,45 @@
-import { View, Text, Pressable, TextInput } from 'react-native';
-import React from 'react';
-import { ADDPICTURES } from '../constants/styles';
-import { ELSTYLES } from '../constants/styles';
+import { View, Text, Pressable, TextInput } from "react-native";
+import React from "react";
+import { ADDPICTURES } from "../constants/styles";
+import { ELSTYLES } from "../constants/styles";
 
 const AddPicsAndInfo = () => {
-    return (
-        <View style={ADDPICTURES.screenWrapper}>
+	return (
+		<View style={ADDPICTURES.screenWrapper}>
+			<View style={ADDPICTURES.contentWrapperTopBottom}>
+				<Text style={ELSTYLES.titleSm}>Add Pics And Info</Text>
+				<Pressable style={ADDPICTURES.roundBtn}>
+					<Text style={ELSTYLES.buttonTxt}>?</Text>
+				</Pressable>
+			</View>
 
-            <View style={ADDPICTURES.contentWrapperTopBottom}>
-                <Text style={ELSTYLES.titleSm}>Add Pics And Info</Text>
-                <Pressable style={ADDPICTURES.roundBtn}>
-                    <Text style={ELSTYLES.buttonTxt}>?</Text>
-                </Pressable>
-                </View>
-            
-            <View style={ADDPICTURES.contentWrapperMiddle}>
-                
-                <View style={ADDPICTURES.innerContentWrapper1}>
-                    <Pressable style={[ELSTYLES.button, ADDPICTURES.addPicsBtn]}>
-                        <Text style={ELSTYLES.buttonTxt}>Camera</Text>
-                    </Pressable>
-                    <Pressable style={[ELSTYLES.button, ADDPICTURES.addPicsBtn]}>
-                        <Text style={ELSTYLES.buttonTxt}>Gallery</Text>
-                    </Pressable>
-                      </View>
-                  
-                  
-                <View style={ADDPICTURES.innerContentWrapper2}>
-                    <TextInput placeholder="Give additional info about the vehicle" style={ELSTYLES.input}></TextInput>
-                </View>
-                </View>
+			<View style={ADDPICTURES.contentWrapperMiddle}>
+				<View style={ADDPICTURES.innerContentWrapper1}>
+					<Pressable style={[ELSTYLES.button, ADDPICTURES.addPicsBtn]}>
+						<Text style={ELSTYLES.buttonTxt}>Camera</Text>
+					</Pressable>
+					<Pressable style={[ELSTYLES.button, ADDPICTURES.addPicsBtn]}>
+						<Text style={ELSTYLES.buttonTxt}>Gallery</Text>
+					</Pressable>
+				</View>
 
-            <View style={ADDPICTURES.contentWrapperTopBottom}>
-                <Pressable style={ADDPICTURES.roundBtn}>
-                    <Text style={ELSTYLES.buttonTxt}>Back</Text>
-                </Pressable>
-                <Pressable style={ADDPICTURES.roundBtn}>
-                    <Text style={ELSTYLES.buttonTxt}>Next</Text>
-                </Pressable>
-            </View>
- 
-        </View>
-    );
+				<View style={ADDPICTURES.innerContentWrapper2}>
+					<TextInput
+						placeholder="Give additional info about the vehicle"
+						style={ELSTYLES.input}></TextInput>
+				</View>
+			</View>
+
+			<View style={ADDPICTURES.contentWrapperTopBottom}>
+				<Pressable style={ADDPICTURES.roundBtn}>
+					<Text style={ELSTYLES.buttonTxt}>Back</Text>
+				</Pressable>
+				<Pressable style={ADDPICTURES.roundBtn}>
+					<Text style={ELSTYLES.buttonTxt}>Next</Text>
+				</Pressable>
+			</View>
+		</View>
+	);
 };
 
-export default AddPicsAndInfo
+export default AddPicsAndInfo;
